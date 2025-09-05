db828caddacd405b447e55d9c7271343f6357764
Surendra <kurmisurendra03@gmail.com>
2025-09-05 11:45:59 +0530

    add comment

--- src/main/java/com/example/demo/FriendController.java
+++ src/main/java/com/example/demo/FriendController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class FriendController {
-
+// hello
     @GetMapping("/add")
     public String validform(Model v)
     {
